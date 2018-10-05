9bdee302a7b2b7062b558b21206b1a091157c520
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-05 17:02:55 +0300

    add some changes

--- task-3-deploy_starter.ps1
+++ task-3-deploy_starter.ps1
@@ -5,8 +5,8 @@ Select-AzureRmSubscription -Subscriptionid $Sub
 
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
 if (!$resourceGroup) {
-    New-AzureRmResourceGroup -Name $ResGroupName -Location 'North Europe'
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location 'North Europe'
 }
 
-$templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-3-MainTemplate.json'
+$templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-3-MainTemplate.json' 
 New-AzureRmResourceGroupDeployment -ResourceGroupName TestRG -TemplateFile $templateFilePath

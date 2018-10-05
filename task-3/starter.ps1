c25e6ea2f26216d1b8a892459d3acec754e3a6d2
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-05 17:37:44 +0300

    change names

--- task-3/starter.ps1
+++ task-3/starter.ps1
@@ -8,5 +8,5 @@ if (!$resourceGroup) {
     New-AzureRmResourceGroup -Name $resourceGroupName -Location 'North Europe'
 }
 
-$templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-3-MainTemplate.json' 
+$templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/main.json' 
 New-AzureRmResourceGroupDeployment -ResourceGroupName TestRG -TemplateFile $templateFilePath

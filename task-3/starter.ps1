b3422451ef6e41d737720f35b7212143691a2ab7
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-05 17:39:14 +0300

    correcting path

--- task-3/starter.ps1
+++ task-3/starter.ps1
@@ -8,5 +8,5 @@ if (!$resourceGroup) {
     New-AzureRmResourceGroup -Name $resourceGroupName -Location 'North Europe'
 }
 
-$templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/main.json' 
+$templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-3/main.json' 
 New-AzureRmResourceGroupDeployment -ResourceGroupName TestRG -TemplateFile $templateFilePath

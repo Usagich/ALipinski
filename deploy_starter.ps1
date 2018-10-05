62a712266abd6ebbb770f1a827be651a62dabd38
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-05 15:35:52 +0300

    3

--- deploy_starter.ps1
+++ deploy_starter.ps1
@@ -1,7 +1,7 @@
-#$Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
+$Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
 $resourceGroupName = "testRG"
 
-#Select-AzureRmSubscription -Subscriptionid $Sub
+Select-AzureRmSubscription -Subscriptionid $Sub
 
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
 if (!$resourceGroup) {

20dfba924ad3e8bac815229be9b0b8d4d762073d
Andrei Lipinski <andrei_lipinski@epam.com>
2018-10-05 15:29:03 +0300

    second commit

--- deploy_starter.ps1
+++ deploy_starter.ps1
@@ -1,4 +1,12 @@
-$Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
-Select-AzureRmSubscription -Subscriptionid $Sub
+#$Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
+$resourceGroupName = "testRG"
+
+#Select-AzureRmSubscription -Subscriptionid $Sub
+
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if (!$resourceGroup) {
+    New-AzureRmResourceGroup -Name $ResGroupName -Location 'North Europe'
+}
+
 $templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/OwnTemplate.json'
 New-AzureRmResourceGroupDeployment -ResourceGroupName TestRG -TemplateFile $templateFilePath

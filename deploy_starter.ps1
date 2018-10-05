39312744b6dcb2e3b7f0cb3380d4fbf23dcd5f25
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-05 14:52:04 +0300

    first update

--- deploy_starter.ps1
+++ deploy_starter.ps1
@@ -1,2 +1,4 @@
+$Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
+Select-AzureRmSubscription -Subscriptionid $Sub
 $templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/OwnTemplate.json'
 New-AzureRmResourceGroupDeployment -ResourceGroupName TestRG -TemplateFile $templateFilePath

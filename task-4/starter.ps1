c920f5d4ae1428d6e67880ea1531756ca25e2372
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-08 13:20:45 +0300

    commit

--- task-4/starter.ps1
+++ task-4/starter.ps1
@@ -1,28 +1,15 @@
 Param(
     [Parameter(Mandatory=$False)]
-    [string]$resourceGroupName = 'task-4-resoure-group'
+    [string]$resourceGroupName = 'TestRG'
 )
-$Location = 'North Europe'
 $Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
-$vaultname = "winadmin"
-$password = "Qwertyu!"
 
 Select-AzureRmSubscription -Subscriptionid $Sub
 
-$resourceGroup = Get-AzureRmResourceGroup `
- -Name $resourceGroupName `
- -ErrorAction SilentlyContinue
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
 if (!$resourceGroup) {
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location $Location
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location 'North Europe'
 }
-
-New-AzureRmKeyVault `
-  -VaultName $vaultname `
-  -ResourceGroupName examplegroup `
-  -Location $Location `
-  -EnabledForTemplateDeployment
-$secretvalue = ConvertTo-SecureString $password -AsPlainText -Force
-Set-AzureKeyVaultSecret -VaultName $vaultname -Name "examplesecret" -SecretValue $secretvalue
 
 $templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-3/main.json' 
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath

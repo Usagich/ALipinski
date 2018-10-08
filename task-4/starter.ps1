eaa26113c5864132501d99db576ca375452a6487
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-08 16:28:04 +0300

    com

--- task-4/starter.ps1
+++ task-4/starter.ps1
@@ -1,2 +1,3 @@
-$templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-4/create-vnet-subnets-pubip-vm.json' 
-New-AzureRmResourceGroupDeployment -ResourceGroupName TestRG1 -TemplateFile $templateFilePath
+$templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-4/1vnet-2subnets-create.json' 
+$templateFilePathPar = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-4/1vnet-2subnets-create-parameters.json'
+New-AzureRmResourceGroupDeployment -ResourceGroupName TestRG2 -TemplateFile $templateFilePath -TemplateParameterFile -$templateFilePathPar

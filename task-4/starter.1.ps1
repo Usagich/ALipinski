cbd23becaece19a5879074ccc2226533e0330b19
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-08 17:42:43 +0300

    com

--- task-4/starter.1.ps1
+++ task-4/starter.1.ps1
@@ -1,15 +1,2 @@
-Param(
-    [Parameter(Mandatory=$False)]
-    [string]$resourceGroupName = 'TestRG2'
-)
-$Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
-
-Select-AzureRmSubscription -Subscriptionid $Sub
-
-$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
-if (!$resourceGroup) {
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location 'West Europe'
-}
-
 $templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-4/main.json' 
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath

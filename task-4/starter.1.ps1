7c4f7590ae04b1ae7c49bf0cad47e22777303d90
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-10 09:02:03 +0300

    push

--- task-4/starter.1.ps1
+++
@@ -1,3 +0,0 @@
-$templateFilePath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-4/create-vnet-subnets-pubip-vm.json'
-$templateFileParamPath = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-4/create-vnet-subnets-pubip-vm-param.json'
-New-AzureRmResourceGroupDeployment -ResourceGroupName TestRG5 -TemplateUri $templateFilePath -TemplateParameterUri $templateFileParamPath

c1f1e6ec5970cd5941faa1821b48945c7eb84951
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-11 20:59:32 +0300

    up

---
+++ Help/download_from_ftp.ps1
@@ -0,0 +1,14 @@
+﻿#ftp server 
+$ftp = "ftp://waws-prod-db3-117.ftp.azurewebsites.windows.net" 
+$user = "XX" 
+$pass = "XXX"
+$SetType = "bin"  
+$remotePickupDir = Get-ChildItem 'c:\test' -recurse
+$webclient = New-Object System.Net.WebClient 
+
+$webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)  
+foreach($item in $remotePickupDir){ 
+    $uri = New-Object System.Uri($ftp+$item.Name) 
+    #$webclient.UploadFile($uri,$item.FullName)
+    $webclient.DownloadFile($uri,$item.FullName)
+}

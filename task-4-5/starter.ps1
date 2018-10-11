7fe9021c6d40caf397859328178e66f863ff1d08
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-11 11:48:45 +0300

    up

--- task-4-5/starter.ps1
+++ task-4-5/starter.ps1
@@ -19,4 +19,4 @@ $accountKeys = Get-AzureRmStorageAccountKey -ResourceGroupName "$ResourceGroupNa
 $storageContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $accountKeys[0].Value
 $sastokenurl = New-AzureStorageBlobSASToken -Container "windows-powershell-dsc" -Blob $dscName -Permission rwl -StartTime (Get-Date).AddHours(-1) -ExpiryTime (get-date).AddMonths(1) -FullUri -Context $storageContext
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateURI -sastokenurl $sastokenurl -TemplateParameterFile $ParametersFilePath
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -sastokenurl $sastokenurl -TemplateUri $templateURI -TemplateParameterFile $ParametersFilePath

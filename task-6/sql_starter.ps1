718f6d1c7eee6914ae893d5de98fc8a92af128b4
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-12 16:41:08 +0300

    up

--- task-6/sql_starter.ps1
+++ task-6/sql_starter.ps1
@@ -1,6 +1,6 @@
 Param(
     [Parameter(Mandatory = $False)]
-    [string]$resourceGroupName = 'Task-6'
+    [string]$resourceGroupName = 'test6'
 )
 $Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
 
@@ -8,13 +8,12 @@ Select-AzureRmSubscription -Subscriptionid $Sub
 
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
 if (!$resourceGroup) {
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location 'West US'
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location 'West Europe'
 }
 
 #$Template = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-6/main.json'
 #$TemplatePar = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-6/main-parameters.json'
 
-$Template = 'C:\git\ALipinski\task-6\main.json'
-$TemplatePar = 'C:\git\ALipinski\task-6\main-parameters.json'
+$Template = 'C:\git\ALipinski\task-6\azure-sql.json'
+$TemplatePar = 'C:\git\ALipinski\task-6\azure-sql-parameters.json'
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $Template -TemplateParameterFile $TemplatePar
-

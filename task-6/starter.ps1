ca3edddbc26029a4cbff9a2865a33cddab7100c0
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-12 16:49:31 +0300

    up

--- task-6/starter.ps1
+++ task-6/starter.ps1
@@ -1,6 +1,6 @@
 Param(
     [Parameter(Mandatory = $False)]
-    [string]$resourceGroupName = 'Task-6'
+    [string]$resourceGroupName = 'task6'
 )
 $Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
 
@@ -8,7 +8,7 @@ Select-AzureRmSubscription -Subscriptionid $Sub
 
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
 if (!$resourceGroup) {
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location 'West US'
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location 'West Europe'
 }
 
 #$Template = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-6/main.json'

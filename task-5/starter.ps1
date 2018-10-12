c3206e6a5afb26608796542cd0dba94bc206299f
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-12 09:28:16 +0300

    final

--- task-5/starter.ps1
+++ task-5/starter.ps1
@@ -1,6 +1,6 @@
 Param(
-    [Parameter(Mandatory=$False)]
-    [string]$resourceGroupName = 'TestRG6'
+    [Parameter(Mandatory = $False)]
+    [string]$resourceGroupName = 'TestRG5'
 )
 $Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
 
@@ -11,7 +11,7 @@ if (!$resourceGroup) {
     New-AzureRmResourceGroup -Name $resourceGroupName -Location 'West US'
 }
 
-$Template = 'C:\git\ALipinski\task-5\main.json'
-$TemplatePar = 'C:\git\ALipinski\task-5\main-parameters.json'
-New-AzureRmResourceGroupDeployment -ResourceGroupName TestRG5 -TemplateFile $Template -TemplateParameterFile $TemplatePar
+$Template = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-5/main.json'
+$TemplatePar = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-5/main-parameters.json'
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $Template -TemplateParameterUri $TemplatePar
 

c747ff900078b53859d9dba2834d8c6d0dfeaf08
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-15 12:01:13 +0300

    first commit

---
+++ task-7/starter.ps1
@@ -0,0 +1,17 @@
+Param(
+    [Parameter(Mandatory = $False)]
+    [string]$resourceGroupName = 'task7'
+)
+Clear-Host
+$Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
+$templateURI = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-7/main.json'
+$templateParametersURI = "https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-7/main-parameters.json"
+
+Select-AzureRmSubscription -Subscriptionid $Sub
+
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if (!$resourceGroup) {
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location 'West Europe'
+}
+
+New-AzureRmResourceGroupDeployment -TemplateUri $templateURI -ResourceGroupName $resourceGroupName -TemplateParameterUri $ParametersFilePath

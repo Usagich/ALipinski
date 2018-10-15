b6b4c6e634c96f396463b0126e6960c623cbb71a
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-15 19:45:14 +0300

    up

---
+++ task-7/new_back_start.ps1
@@ -0,0 +1,3 @@
+﻿$Vault = Get-AzureRmBackupVault -Name "MyRSVault90"
+$Container = Get-AzureRmBackupContainer -Vault $Vault -Type AzureVM -Name "vm1-task7"
+Get-AzureRmBackupItem -Container $Container | Backup-AzureRmBackupItem

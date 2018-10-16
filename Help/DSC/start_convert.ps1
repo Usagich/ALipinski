5e6ecb039c5aacde63698977dbdf742b069652ca
AzureLabDevOps <43855832+AzureLabDevOps@users.noreply.github.com>
2018-10-16 19:04:00 +0300

    Update start_convert.ps1

--- Help/DSC/start_convert.ps1
+++ Help/DSC/start_convert.ps1
@@ -1,1 +1,2 @@
 Publish-AzureRmVMDscConfiguration -ConfigurationPath "C:\git\ALipinski\Help\DSC\dsc-all.ps1" -ResourceGroupName TestRG5 -StorageAccountName andreilab
+#Publish-AzureRmVMDscConfiguration -ConfigurationPath "C:\git\ALipinski\task-7\dsc.ps1" -OutputArchivePath "C:\git\ALipinski\task-7\dsc.ps1.zip"

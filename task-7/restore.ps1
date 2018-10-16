f72f470b34931637acbe2d612982ff5901906e41
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-16 19:22:14 +0300

    update

---
+++ task-7/restore.ps1
@@ -0,0 +1,49 @@
+Param(
+    [Parameter(Mandatory = $False)]
+    [string]$resourceGroupName = 'task7restore'
+)
+$location = 'West Europe'
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if (!$resourceGroup) {
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+}
+
+$storageAccountName = "task7restorestoracc"
+$skuName = "Standard_LRS"
+
+
+$storage = Get-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+if (!$storage) {
+    New-AzureRmStorageAccount -ResourceGroupName $resourceGroupName `
+        -Name $storageAccountName `
+        -Location $location `
+        -SkuName $skuName
+}
+
+
+# Create the storage account.
+$storageAccount = New-AzureRmStorageAccount -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Location $location `
+    -SkuName $skuName
+
+
+
+$vault = Get-AzureRmRecoveryServicesVault
+Set-AzureRmRecoveryServicesVaultContext -Vault $vault
+
+$namedContainer = Get-AzureRmRecoveryServicesBackupContainer `
+    -ContainerType "AzureVM" `
+    -Status "Registered" `
+    -FriendlyName (get-azurermvm).name
+
+$item = Get-AzureRmRecoveryServicesBackupItem `
+    -Container $namedContainer `
+    -WorkloadType "AzureVM"
+
+$rp = Get-AzureRmRecoveryServicesBackupRecoveryPoint -Item $Item 
+
+$RestoreJob = Restore-AzureRmRecoveryServicesBackupItem `
+    -RecoveryPoint $RP[0] `
+    -StorageAccountName $StorageAccountName `
+    -StorageAccountResourceGroupName $resourceGroupName

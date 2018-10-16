e1e70361a1fce3b6d5eb0f6bf2eba442845749d0
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-16 15:04:45 +0300

    update

--- task-7/new_back_start.ps1
+++
@@ -1,8 +0,0 @@
-﻿$Vault = Get-AzureRmBackupVault -Name "MyRSVault90"
-$Container = Get-AzureRmBackupContainer -Vault $Vault -Type AzureVM -Name "vm1-task7"
-Get-AzureRmBackupItem -Container $Container | Backup-AzureRmBackupItem
-
-$backupcontainer = Get-AzureRmRecoveryServicesBackupContainer `
-    -ContainerType "AzureVM" `
-    -FriendlyName "MyRSVault90"
-

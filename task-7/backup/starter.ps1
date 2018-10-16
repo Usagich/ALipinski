9493093d53c48c06191f160a8660625836676e03
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-16 15:05:11 +0300

    update

--- task-7/backup/starter.ps1
+++ task-7/backup/starter.ps1
@@ -6,6 +6,23 @@ Clear-Host
 $Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
 $templateURI = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-7/main.json'
 $templateParametersURI = "https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-7/main-parameters.json"
+$vm = Get-AzureRmVM -ResourceGroupName $resourceGroupName
+$vault = get-AzureRmRecoveryServicesVault -ResourceGroupName $resourceGroupName
+
+Write-Host "Please enter login name for VM: "
+$login = Read-Host
+
+#generate random number for backup vault name
+$UTCNow = (Get-Date).ToUniversalTime()
+$random = $UTCNow.Millisecond
+
+Write-Host "Please enter password for VM: "
+$password = Read-Host -AsSecureString
+
+$ParametersFilePath = "$env:TEMP\main-parameters.json"
+
+#Download from URI to %temp%
+Invoke-WebRequest -Uri $templateParametersURI -OutFile $ParametersFilePath
 
 Select-AzureRmSubscription -Subscriptionid $Sub
 
@@ -14,4 +31,25 @@ if (!$resourceGroup) {
     New-AzureRmResourceGroup -Name $resourceGroupName -Location 'West Europe'
 }
 
-New-AzureRmResourceGroupDeployment -TemplateUri $templateURI -ResourceGroupName $resourceGroupName -TemplateParameterUri $templateParametersURI
+New-AzureRmResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateUri $templateURI `
+    -login $login `
+    -password $password `
+    -random $random `
+    -TemplateParameterFile $ParametersFilePath `
+    -Verbose
+
+
+ 
+Set-AzureRmRecoveryServicesVaultContext -Vault $vault
+
+$namedContainer = Get-AzureRmRecoveryServicesBackupContainer `
+    -ContainerType "AzureVM" `
+    -Status "Registered" `
+    -FriendlyName $vm.Name
+
+$item = Get-AzureRmRecoveryServicesBackupItem -Container $namedContainer `
+    -WorkloadType "AzureVM"
+
+$job = Backup-AzureRmRecoveryServicesBackupItem -Item $item

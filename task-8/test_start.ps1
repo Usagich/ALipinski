1c721f5f346dc942bbc52ddf693b60d7663b6cb9
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-17 18:12:15 +0300

    update

---
+++ task-8/test_start.ps1
@@ -0,0 +1,29 @@
+Param(
+    [Parameter(Mandatory = $False)]
+    [string]$resourceGroupName = 'task8'
+)
+$location = 'West europe'
+$Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
+$template = "C:\git\ALipinski\task-8\aks.json"
+
+#Select subscription
+Select-AzureRmSubscription -Subscriptionid $Sub
+
+#Check resource group name
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if (!$resourceGroup) {
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+}
+
+#get clientid and secret fron KeyVault
+$KeyVault = (Get-AzureRmKeyVault | where {$_.VaultName -like '*task8'}).VaultName
+
+$ClientId = (Get-AzureKeyVaultSecret -VaultName $KeyVault -Name ClientId).SecretValue
+$secret = (Get-AzureKeyVaultSecret -VaultName $KeyVault -Name secret).SecretValue
+
+
+New-AzureRmResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -ClientId $ClientId `
+    -secret $secret 

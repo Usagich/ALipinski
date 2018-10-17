2ef6f8e8bb7f4960b6f1dd6a7e8dc225fb17976d
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-17 17:12:20 +0300

    update

--- task-8/test.ps1
+++ task-8/test.ps1
@@ -9,7 +9,7 @@ if (!$resourceGroup) {
     New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
 }
 $appid = 'bfcb2a07-de28-4b77-b704-e2d5eb5395a1'
-$apppass = (Get-AzureKeyVaultSecret -VaultName keytask8 -Name apppass).SecretValueText
+$apppass = (Get-AzureKeyVaultSecret -VaultName keytask8 -Name apppass).SecretValue
 $template = "C:\git\ALipinski\task-8\kbs.json"
 
 

86a5056e2de3344e5b03a91d64656fcebe25ae95
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-19 13:47:38 +0300

    update

---
+++ task-9/starter.ps1
@@ -0,0 +1,40 @@
+Param(
+    [Parameter(Mandatory = $False)]
+    [string]$resourceGroupName = 'task9'
+)
+Clear-Host
+$Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
+$template = 'C:\git\ALipinski\task-9\vm_and_network.json'
+
+Select-AzureRmSubscription -Subscriptionid $Sub
+
+# #Enter login name
+# Write-Host "Please enter login name for VM: "
+# $login = Read-Host
+# #Enter password for VM
+# Write-Host "Please enter password for VM: "
+# $password = Read-Host -AsSecureString
+
+# $ParametersFilePath = "$env:TEMP\main-parameters.json"
+
+# #Download from URI to %temp%
+# Invoke-WebRequest -Uri $templateParametersURI -OutFile $ParametersFilePath
+
+
+#Check resource group name
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if (!$resourceGroup) {
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+}
+
+#Deploy main template
+New-AzureRmResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateUri $templateURI `
+    # -login $login `
+    # -password $password `
+    # -TemplateParameterFile $ParametersFilePath `
+    -Verbose
+
+#remove min-parameters file from temp folder
+# Remove-Item $ParametersFilePath

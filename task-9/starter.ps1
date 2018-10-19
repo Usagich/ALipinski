e927a3c07af8f68b101f855a67ce6c8c4779d5d0
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-19 15:59:29 +0300

    update

--- task-9/starter.ps1
+++ task-9/starter.ps1
@@ -4,7 +4,7 @@ Param(
 )
 Clear-Host
 $Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
-$template = 'C:\git\ALipinski\task-9\vm_and_network.json'
+# $template = 'C:\git\ALipinski\task-9\vm_and_network.json'
 $location = 'West Europe'
 
 Select-AzureRmSubscription -Subscriptionid $Sub
@@ -27,15 +27,21 @@ $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction 
 if (!$resourceGroup) {
     New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
 }
-
-#Deploy main template
-New-AzureRmResourceGroupDeployment `
-    -ResourceGroupName $resourceGroupName `
-    -TemplateFile $template `
-    # -login $login `
-    # -password $password `
-    # -TemplateParameterFile $ParametersFilePath `
-    -Verbose
+# #Deploy main template
+# New-AzureRmResourceGroupDeployment `
+#     -ResourceGroupName $resourceGroupName `
+#     -TemplateFile $template `
+#     # -login $login `
+#     # -password $password `
+#     # -TemplateParameterFile $ParametersFilePath `
+#     -Verbose
 
 #remove min-parameters file from temp folder
 # Remove-Item $ParametersFilePath
+$template = "C:\git\ALipinski\task-9\create-automation-account.json"
+$templatePar = "C:\git\ALipinski\task-9\create-automation-account-parameters.json"
+
+New-AzureRmResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -TemplateParameterFile $templatePar

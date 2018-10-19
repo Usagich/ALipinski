b0b785b1a76dd94cbd67f442279ab32a725096ca
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-10-19 22:19:50 +0300

    update

--- task-9/starter.ps1
+++ task-9/starter.ps1
@@ -2,35 +2,120 @@ Param(
     [Parameter(Mandatory = $False)]
     [string]$resourceGroupName = 'task9'
 )
+Clear-Host
+$Sub = "1f1fe2e5-5f13-4687-aef3-063acc693dd3"
+Select-AzureRmSubscription -Subscriptionid $Sub
 
-$template = "C:\git\ALipinski\task-9\vm_and_network.json"
-
-New-AzureRmResourceGroupDeployment `
-    -ResourceGroupName $resourceGroupName `
-    -TemplateFile $template `
-    -Verbose
-
-Set-AzureRMStorageBlobContent - "https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-9/Workflow_Stop-AzureVM.ps1" `
-    -Container $containerName `
-    -Blob "Image001.jpg" `
-    -Context $ctx 
-
+$storageAccountName = 'task9storage'
+$containerName = "task9"
+$blob_name = "Workflow_Stop-AzureVM.ps1"
+$skuName = "Standard_LRS"
+$location = 'West Europe'
+$templateURI = 'https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-9/main.json'
 $workflowURI = "https://raw.githubusercontent.com/AzureLabDevOps/ALipinski/master/task-9/Workflow_Stop-AzureVM.ps1"
 $workflow = "$env:TEMP\Workflow_Stop-AzureVM.ps1"
-
+#Download from URI to %temp%
 Invoke-WebRequest -Uri $workflowURI -OutFile $workflow
 
-$storageAccount = get-AzureRmStorageAccount -ResourceGroupName task9 `
-  -Name "task9storage" 
+$resourceGroup = Get-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -ErrorAction SilentlyContinue
 
-$ctx = $storageAccount.Context
+if (!$resourceGroup) {
+    New-AzureRmResourceGroup `
+        -Name $resourceGroupName `
+        -Location 'West Europe'
+}
 
+$storageAccount = Get-AzureRmStorageAccount `
+    -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -ErrorAction SilentlyContinue
 
-$containerName = "quickstartblobs"
-New-AzureStorageContainer -Name $containerName -Context $ctx -Permission blob
+if (!$storageAccount) {
+    New-AzureRmStorageAccount `
+        -ResourceGroupName $resourceGroupName `
+        -Name $storageAccountName `
+        -Location $location `
+        -SkuName $skuName
+}
 
+$storageAccountContext = (get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName).Context
+New-AzureStorageContainer -Name $containerName -Context $storageAccountContext -Permission blob -ErrorAction SilentlyContinue
 Set-AzureStorageBlobContent -file  $workflow `
   -Container $containerName `
-  -Blob "Workflow_Stop-AzureVM.ps1" `
-  -Context $ctx 
+  -Blob $blob_name `
+  -Context $storageAccountContext `
+  -Force
+
+Remove-Item $workflow
+
+$accountKeys = Get-AzureRmStorageAccountKey `
+    -ResourceGroupName "$ResourceGroupName" `
+    -Name "$StorageAccountName"
+
+$storageContext = New-AzureStorageContext `
+    -StorageAccountName $StorageAccountName `
+    -StorageAccountKey $accountKeys[0].Value
+
+$sastokenurl = ConvertTo-SecureString -AsPlainText (New-AzureStorageBlobSASToken `
+    -Container $containerName `
+    -Blob $blob_name -Permission rwl `
+    -StartTime (Get-Date).AddHours(-1) `
+    -ExpiryTime (get-date).AddMonths(1) `
+    -FullUri -Context $storageContext) -Force
+
+$jobid = [System.Guid]::NewGuid().toString()
+
+#Enter login name for VM
+Write-Host "Please enter login name for VM: "
+$vm_login = Read-Host
+
+#Enter password for VM
+Write-Host "Please enter password for VM: "
+$vm_passwd = Read-Host -AsSecureString
+
+#Enter password for app registration secret
+Write-Host "Enter password for app registration secret: "
+$app_pass = Read-Host -AsSecureString
+
+$application = Get-AzureRmADApplication | `
+    Where-Object {$_.HomePage -like "http://task9.com"}
+    
+$obj_id = $application.ObjectId
+
+$app_id = ($application.ApplicationId).Guid
+
+$app_key = Get-AzureRmADAppCredential `
+    -ObjectId $obj_id `
+    -ErrorAction SilentlyContinue
+
+if ($app_key) {
+    Remove-AzureRmADAppCredential `
+        -ObjectId $obj_id `
+        -Force
+
+    New-AzureRmADAppCredential `
+        -ObjectId $obj_id `
+        -Password $app_pass
+}
+else {
+    New-AzureRmADAppCredential `
+        -ObjectId $obj_id `
+        -Password $app_pass 
+}
+
+New-AzureRmResourceGroupDeployment `
+    -TemplateUri $templateURI `
+    -ResourceGroupName $resourceGroupName `
+    -vm_login $vm_login `
+    -vm_passwd $vm_passwd `
+    -app_id $app_id `
+    -app_pass $app_pass `
+    -jobid $jobid `
+    -sastokenurl $sastokenurl `
+    -Verbose
+
+
+
 

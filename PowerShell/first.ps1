a2e3b25d3016b74bab806cf02abb664f1aaa7d4f
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-11-06 10:41:15 +0300

    up

--- PowerShell/first.ps1
+++ PowerShell/first.ps1
@@ -44,3 +44,25 @@ function speaker {
     $voice = new-object -comobject "SAPI.SPVoice"
     $voice.speak("happy $day $($text.Content)")
 }
+
+Get-WmiObject -Class Win32_OperatingSystem -Namespace root/cimv2 `
+    -ComputerName . | Format-List * 
+
+
+workflow hello {
+    'Hello World'
+}
+
+workflow Invoke-ParallelForEach {
+    foreach -parallel ($i in 1..10) {
+        InlineScript 
+        {
+            "foo: $using:i"
+        }
+        $count = Get-Process -Name PowerShell* | Measure-Object | Select-Object -ExpandProperty Count
+        "Number of PowerShell processes = $count"
+    }
+} 
+$startcount = Get-Process -Name PowerShell* | Measure-Object |Select-Object -ExpandProperty Count
+"Number of starting PowerShell processes = $startcount"
+Invoke-ParallelForEach

77adc5c50e4a981f7be86630377937ecc1f08013
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-11-13 15:54:28 +0300

    commit

--- PowerShell/Module-3/task-2.ps1
+++ PowerShell/Module-3/task-2.ps1
@@ -1,28 +1,20 @@
-$main = @{
-    Computers = @()
-}
 function Get-Info {
     $FreeSpace = (Get-PSDrive C).Free / 1gb
     $TopProcess = (Get-Process | Sort-Object CPU -Descending | select -First 5).Processname
     $loadCpu = (Get-WmiObject win32_processor | select LoadPercentage).LoadPercentage
+
     $Computers += [Ordered]@{
-        ComputerName = "$env:COMPUTERNAME";
-        FreeSpace    = $FreeSpace;
-        TopProcess   = $TopProcess;
-        loadCpu      = $loadCpu
+        FreeSpace  = $FreeSpace;
+        TopProcess = $TopProcess;
+        loadCpu    = $loadCpu
     }
-#     $Computers = @"
-#         ComputerName = "$env:COMPUTERNAME";
-#         FreeSpace    = $FreeSpace;
-#         TopProcess   = $TopProcess;
-#         loadCpu      = $loadCpu
-# "@
     return $Computers
 }
 
 [string]$textPath = '.\cn.txt'
 $ComputersList = (Get-Content $textPath).Split("`n|,| ")
-$per = Invoke-Command -ThrottleLimit 2 -ComputerName $ComputersList -ScriptBlock ${function:Get-Info}
-$new | ConvertTo-Json
+Invoke-Command -ThrottleLimit 2 -ComputerName $ComputersList `
+    -ScriptBlock ${function:Get-Info} -OutVariable var 
+#| ConvertTo-Json | Out-File .\get-info.json
 
-$per | ConvertTo-Json
+$var |select -ExcludeProperty RunspaceId,PSShowComputerName | ConvertTo-Json | Out-File .\get-info.json
--- PowerShell/Module-3/task-3.ps1
+++ PowerShell/Module-3/task-3.ps1
@@ -1,19 +1,20 @@
-$path = 'C:\git'
+$path = $env:TEMP
+$JobsCount = 8
 $folders = (Get-ChildItem $path  | ? { $_.PSIsContainer}).FullName
+
 foreach ($folder in $folders) {
     $running = Get-Job | Where-Object { $_.State -eq 'Running' }
-    if ($running.Count -lt 3) {
+    if ($running.Count -lt $JobsCount) {
         Start-Job -ScriptBlock {
             param ($folder)
             Set-Location $folder
-            Get-ChildItem $folder
+            Get-ChildItem $folder -Recurse
         } -Arg "$folder"
     }
     else {
         $running | Wait-Job 
     }
+    $param += @((Get-Job -State Completed | Receive-Job).FullName)
+    Get-Job -State Completed | remove-Job
 }
-
-Get-Job -State Completed | Receive-Job
-Get-Job  | remove-Job
-
+$param

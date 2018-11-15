62a000baf21b5593901e14f34dfcb8bf179e9b25
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-11-15 19:29:29 +0300

    update

--- PowerShell/Module-4/dev-task-3.ps1
+++ PowerShell/Module-4/dev-task-3.ps1
@@ -4,12 +4,13 @@ $site = "https://github.com/trending"
 [Reflection.Assembly]::LoadFile($path)|Out-Null
 [HtmlAgilityPack.HtmlWeb]$web = @{}
 [HtmlAgilityPack.HtmlDocument]$doc = $web.LoadFromBrowser("https://github.com/trending") 
-
+[HtmlAgilityPack.HtmlNodeCollection]$NameHtml = $doc.DocumentNode.SelectNodes("//div[@class='d-inline-block col-9 mb-1']/h3/a/text()")
 [HtmlAgilityPack.HtmlNodeCollection]$AddressHtml = $doc.DocumentNode.SelectNodes("//div[@class='d-inline-block col-9 mb-1']/h3/a")
 [HtmlAgilityPack.HtmlNodeCollection]$LanguageHtml = $doc.DocumentNode.SelectNodes("//span[@itemprop='programmingLanguage']")
 [HtmlAgilityPack.HtmlNodeCollection]$StarsTotalHtml = $doc.DocumentNode.SelectNodes("//div[@class='f6 text-gray mt-2']/a[1]/text()")
 [HtmlAgilityPack.HtmlNodeCollection]$StarsTodayHtml = $doc.DocumentNode.SelectNodes("//div[@class='f6 text-gray mt-2']/span[3]/text()")
 
+$Name = $NameHtml.Text
 $Address = $AddressHtml.InnerText
 $Language = $LanguageHtml.InnerText
 $StarsTotal = $StarsTotalHtml.Text
@@ -17,8 +18,9 @@ $StarsToday = $StarsTodayHtml.Text
 
 $main = @()
 
-for ($i = 0; $i -lt $Address.Length; $i++) {
+for ($i = 0; $i -lt $Name.Length; $i++) {
     $main += [ordered]@{
+        Name       = $Name[$i];
         Address    = $Address[$i];
         Language   = $Language[$i];
         StarsTotal = $StarsTotal[$i];

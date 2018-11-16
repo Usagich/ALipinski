6f05bb5aeec3f9c1ad305f4c58401da6b998dba6
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-11-16 16:17:02 +0300

    up

--- PowerShell/Module-4/task-1.ps1
+++ PowerShell/Module-4/task-1.ps1
@@ -1,4 +1,4 @@
-$log = ".\dism.log"
+$log = "c:\Windows\Logs\DISM\DISM.log"
 $errors = (Get-Content $log).Split("`n") | where {$_ -match "^(.{21})error"}
 foreach ($item in $errors) {
     if (($item -match "(?<=hr:0x).*\+?(?=\))") -or ($item -match "(?<=HRESULT\=).*$")) {

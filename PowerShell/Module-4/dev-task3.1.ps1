df5dc3f1244515b1640dc72c8a3ca2c60d016abc
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-11-16 13:26:45 +0300

    up

--- PowerShell/Module-4/dev-task3.1.ps1
+++ PowerShell/Module-4/dev-task3.1.ps1
@@ -1,20 +1,21 @@
-$dll = "$env:temp\HtmlAgilityPack.dll"
-Invoke-WebRequest -Uri "https://github.com/AzureLabDevOps/ALipinski/raw/master/PowerShell/Module-4/HtmlAgilityPack.dll" -OutFile $dll
-$site = "https://github.com/trending"
+# $dll = "$env:temp\HtmlAgilityPack.dll"
+# Invoke-WebRequest -Uri "https://github.com/AzureLabDevOps/ALipinski/raw/master/PowerShell/Module-4/HtmlAgilityPack.dll" -OutFile $dll
+# $site = "https://github.com/trending"
 
-[Reflection.Assembly]::LoadFile($dll)|Out-Null
+$path ="C:\git\ALipinski\PowerShell\Module-4\HtmlAgilityPack.dll"
+
+[Reflection.Assembly]::LoadFile($path)|Out-Null
 [HtmlAgilityPack.HtmlWeb]$web = @{}
 [HtmlAgilityPack.HtmlDocument]$doc = $web.LoadFromBrowser($site) 
-[HtmlAgilityPack.HtmlNodeCollection]$nodes = $doc.DocumentNode.SelectNodes("//li[@class='col-12 d-block width-full py-4 border-bottom']")
+[HtmlAgilityPack.HtmlNodeCollection]$nodes = $doc.DocumentNode.SelectNodes("//html/body/div[4]/div[2]/div/div[1]/div[2]/ol/*")
 
 $main = @{
     GitHub_Trends = @()
 }
-
 for ($i = 0; $i -lt $nodes.Count; $i++) {
-    $Name = ($nodes[$i].ChildNodes[1].ChildNodes[1].InnerText).replace(" ", '')
+    $Name = (($nodes[$i].InnerText.Split("`n"))[2]).Replace(" ","").Trim()
     $Address = "https://github.com/$Name"
-    ($nodes[0].InnerText.Split("`n"))[6] -match "((.*)\s)?(.*\d)\s(.*\d)\s(.*\w)\s(.*\w)(.*\s)(.*\d)" | Out-Null
+    ($nodes[$i].InnerText.Split("`n"))[6] -match "((.*)\s)?(.*\d)\s(.*\d)\s(.*\w)\s(.*\w)(.*\s)(.*\d)" | Out-Null
     $Language = $Matches[1]
     $Starstoday = $Matches[8]
     $Starstotal = $Matches[3]
@@ -38,4 +39,3 @@ for ($i = 0; $i -lt $nodes.Count; $i++) {
 }
 $main | ConvertTo-Json | Out-File .\github.json
 
-Remove-Item $dll

d31966d8b38970e053aa30aa272a630e1ad81c7f
Andrei Lipinski <Andrei_Lipinski@epam.com>
2018-11-19 13:38:08 +0300

    up

--- PowerShell/Module-5/ConfigurationData.psd1
+++ PowerShell/Module-5/ConfigurationData.psd1
@@ -6,10 +6,11 @@
             GitLastUri = 'https://github.com/git-for-windows/git/releases/download/v2.19.1.windows.1/PortableGit-2.19.1-64-bit.7z.exe'
             GitOldUri = 'https://github.com/git-for-windows/git/releases/download/v2.18.0-rc1.windows.2/PortableGit-2.18.0.rc1.windows.2-64-bit.7z.exe'
             JreUri = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=235727_2787e4a523244c269598db4e85c51e0c'
-            zipOut = 'C:\soft\Download\7z1805-x64.exe'
-            GitLastOut = 'C:\soft\Download\PortableGit-2.19.1-64-bit.7z.exe'
-            GitOldOut = 'C:\soft\Download\PortableGit-2.18.0.rc1.windows.2-64-bit.7z.exe'
-            JreOut = 'C:\soft\Download\JRE.msi'
+            zipOut = '7z1805-x64.exe'
+            GitLastOut = 'PortableGit-2.19.1-64-bit.7z.exe'
+            GitOldOut = 'PortableGit-2.18.0.rc1.windows.2-64-bit.7z.exe'
+            JreOut = 'JRE.msi'
+            Out = 'C:\soft'
         }    
     )
 }
--- PowerShell/Module-5/dev-task-1.ps1
+++ PowerShell/Module-5/dev-task-1.ps1
@@ -16,63 +16,77 @@ Configuration InstallAll
                 }
             }
         }    
-        Script DownloadMsi {
+        Script DownloadInstalls {
             GetScript  = 
             {
                 @{
-                    Result     = ('True' -in (Test-Path $AllNodes.JreOut))
+                    Result     = ('True' -in (Test-Path "$($using:AllNodes.Out)\$($using:AllNodes.JreOut)"))
                 }
             }    
             SetScript  = 
             {
-                Invoke-WebRequest -Uri $AllNodes.zipUri -OutFile $AllNodes.zipOut           
-                Invoke-WebRequest -Uri $AllNodes.GitLastUri  -OutFile $AllNodes.GitLastOut 
-                Invoke-WebRequest -Uri $AllNodes.GitOldUri -OutFile $AllNodes.GitOldOut
-                Invoke-WebRequest -Uri $AllNodes.JreUri -OutFile $AllNodes.JreOut
+                Invoke-WebRequest -Uri $using:AllNodes.zipUri -OutFile "$($using:AllNodes.Out)\$($using:AllNodes.zipOut)"          
+                Invoke-WebRequest -Uri $using:AllNodes.GitLastUri  -OutFile "$($using:AllNodes.Out)\$($using:AllNodes.GitLastOut)"
+                Invoke-WebRequest -Uri $using:AllNodes.GitOldUri -OutFile "$($using:AllNodes.Out)\$($using:AllNodes.GitOldOut)"
+                Invoke-WebRequest -Uri $using:AllNodes.JreUri -OutFile "$($using:AllNodes.Out)\$($using:AllNodes.JreOut)"
       
             }        
             TestScript = 
             {
-                $Status = ('True' -in (Test-Path $AllNodes.JreOut))
+                $Status = ('True' -in (Test-Path "$($using:AllNodes.Out)\$($using:AllNodes.JreOut)"))
                 $Status -eq $True
             }
         }
-        # Package JRE {
-        #     Ensure    = "Present"
-        #     Name      = "Java Platform SE 8 U191"
-        #     Path      = "C:\soft\JRE.msi"
-        #     ProductId = ""
-        #     Arguments = "/s INSTALLDIR=`"C:\soft\Java`" STATIC=1"
-        # }
-        # Environment  JAVA_HOME {
-        #     Name      = "JAVA_HOME"
-        #     Ensure    = "Present"
-        #     Path      = $true
-        #     Value     = "C:\soft\Java"
-        #     DependsOn = "[Package]JRE"
-        # }
-        # Package 7-Zip {
-        #     Ensure    = "Present"
-        #     Name      = "7-Zip 18.05 (x64 edition)"
-        #     Path      = "C:\soft\7z1805-x64.msi"
-        #     ProductId = "23170F69-40C1-2702-1805-000001000000"
-        #     Arguments = "INSTALLDIR=`"C:\soft\7-Zip`""
-        # }
+        Package JRE {
+            Ensure    = "Present"
+            Name      = "Java Platform SE 8 U191"
+            Path      = "$($AllNodes.Out)\$($AllNodes.JreOut)"
+            ProductId = ""
+            Arguments = "/s INSTALLDIR=`"$($AllNodes.Out)\Java`" STATIC=1"
+        }
+        Environment  JAVA_HOME {
+            Name      = "JAVA_HOME"
+            Ensure    = "Present"
+            Path      = $true
+            Value     = "$($AllNodes.Out)\Java"
+            DependsOn = "[Package]JRE"
+        }
+        Package 7zip {
+            Ensure    = "Present"
+            Name      = "7-Zip 18.05 (x64 edition)"
+            Path      = "$($AllNodes.Out)\$($AllNodes.zipOut)"
+            ProductId = "23170F69-40C1-2702-1805-000001000000"
+            Arguments = "INSTALLDIR=`"$($AllNodes.Out)\7-Zip`""
+        }
         # WindowsProcess ExtractGit_Latest {
-        #     Path      = "C:\soft\7-Zip\7z.exe"
-        #     Arguments = "x c:\soft\PortableGit-2.19.1-64-bit.7z.exe -oc:\soft\Git_Latest -y"
+        #     Path      = "$($using:AllNodes.Out)\7-Zip\7z.exe"
+        #     Arguments = "x $($using:AllNodes.Out)\PortableGit-2.19.1-64-bit.7z.exe -o$($using:AllNodes.Out)\Git_Latest -y"
         #     DependsOn = "[Package]7-Zip"
         # }
         # WindowsProcess ExtractGit_Old {
-        #     Path      = "C:\soft\7-Zip\7z.exe"
-        #     Arguments = "x c:\soft\PortableGit-2.17.0-64-bit.7z.exe -oc:\soft\Git_Old -y"
+        #     Path      = "$($using:AllNodes.Out)\7-Zip\7z.exe"
+        #     Arguments = "x $($using:AllNodes.Out)\PortableGit-2.17.0-64-bit.7z.exe -o$($using:AllNodes.Out)\Git_Old -y"
         #     DependsOn = "[Package]7-Zip"
         # }
+        Package Git1 {
+            Ensure    = "Present"  
+            Path      = "$($AllNodes.Out)\7-Zip\7z.exe"
+            Name      = "GItlatest"
+            ProductId = ''
+            dependson = '[Package]7zip'
+            Arguments = "x $($AllNodes.Out)\$($AllNodes.GitLastOut) -o$($AllNodes.Out)\GitLastVersion"
+        }
+        Package Git2 {
+            Ensure    = "Present"  
+            Path      = "$($AllNodes.Out)\7-Zip\7z.exe"
+            Name      = "GIT"
+            ProductId = ''
+            dependson = '[Package]7zip'
+            Arguments = "x $($AllNodes.Out)\$($AllNodes.GitOldOut) -o$($AllNodes.Out)\GitOldVersion"
+        }
     }
 }
 
-$EnvironmentData = Import-PowerShellDataFile -Path .\Module-5\ConfigurationData.psd1
-
-InstallAll -OutputPath "C:\soft\mof" -ConfigurationData $EnvironmentData
+InstallAll -ConfigurationData .\ConfigurationData.psd1 -OutputPath C:\soft\mof
 
 Start-DscConfiguration -Path "C:\soft\mof" -ComputerName localhost -Force -Verbose -wait
